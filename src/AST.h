937d897179630c6ce5132b5bfeabdfe92e857279
zazemlil <tiknik19@mail.ru>
2025-10-22 03:35:53 +0300

    refactoring AST.h and output in main.cpp

--- src/AST.h
+++ src/AST.h
@@ -7,7 +7,6 @@
 namespace syntax_tree {
 
 class ASTNode {
-private:
     std::string node_type;
     std::vector<std::shared_ptr<ASTNode>> statements;
 
@@ -17,14 +16,15 @@ public:
     
     virtual ~ASTNode() = default;
 
-    std::string getNodeType() const {
-        return node_type;
-    }
+    std::string getNodeType() const { return node_type; }
+    size_t getStatementCount() const { return statements.size(); }
 
-    void addStatement(std::shared_ptr<ASTNode> stmt) {
-        statements.push_back(stmt);
-    }
+    std::shared_ptr<ASTNode> getStatement(size_t index) const { return statements.at(index); }
+    void addStatement(std::shared_ptr<ASTNode> stmt) { statements.push_back(stmt); }
 
+    void setStatements(std::vector<std::shared_ptr<ASTNode>> new_statements) { this->statements = new_statements; }
+    std::vector<std::shared_ptr<ASTNode>> getStatements() const { return statements; }
+    
     void addStatements(std::vector<std::shared_ptr<ASTNode>> new_statements) {
         this->statements.insert(
             std::end(this->statements),
@@ -33,31 +33,13 @@ public:
         );
     }
 
-    void setStatements(std::vector<std::shared_ptr<ASTNode>> new_statements) {
-        this->statements = new_statements;
-    }
-    
-    size_t getStatementCount() const { 
-        return statements.size(); 
-    }
-
-    std::shared_ptr<ASTNode> getStatement(size_t index) const { 
-        return statements.at(index);
-    }
-
-    std::vector<std::shared_ptr<ASTNode>> getStatements() const { 
-        return statements;
-    }
-
-    virtual void printValue(bool flat = false) const {
-        if (!flat) std::cout << node_type << " ";
-    }
+    virtual void printValue() const { std::cout << node_type; }
 
     void print(int indent = 0) const {
-        std::string indentStr = ""; // "|"
-        for (int i = 0; i < indent-1; i++) {indentStr += "    ";} // "    |"
+        std::string indentStr = ""; 
+        for (int i = 0; i < indent-1; i++) {indentStr += "    ";} 
         
-        std::cout << indentStr << ""; // "--- "
+        std::cout << indentStr << "";
         this->printValue();
         std::cout << '\n';
         
@@ -66,190 +48,23 @@ public:
         }
     }
 
-    virtual void printFlat() const {
-        this->printValue(true);
-        //std::cout << ' ';
+    virtual void printFlat(int depth = 0) const {
+        if (!statements.empty()) {
+            std::cout << "(";
+        }
         
-        for (const auto& stmt : statements) {
-            stmt->printFlat();
-        }
-    }
-};
-
-
-// unary
-class QuoteNode : public ASTNode {
-public:
-    void printValue(bool flat = false) const override {
-        std::cout << getNodeType();
-    }
-
-    QuoteNode(std::string t) : ASTNode(t) {}
-};
-
-class CarNode : public ASTNode {
-public:
-    CarNode(std::string t) : ASTNode(t) {}
-};
-
-class CdrNode : public ASTNode {
-public:
-    CdrNode(std::string t) : ASTNode(t) {}
-};
-
-class AtomNode : public ASTNode {
-public:
-    AtomNode(std::string t) : ASTNode(t) {}
-};
-
-
-// binary
-class AddNode : public ASTNode {
-public:
-    AddNode(std::string t) : ASTNode(t) {}
-};
-
-class SubNode : public ASTNode {
-public:
-    SubNode(std::string t) : ASTNode(t) {}
-};
-
-class MulNode : public ASTNode {
-public:
-    MulNode(std::string t) : ASTNode(t) {}
-};
-
-class DiveNode : public ASTNode {
-public:
-    DiveNode(std::string t) : ASTNode(t) {}
-};
-
-class RemNode : public ASTNode {
-public:
-    RemNode(std::string t) : ASTNode(t) {}
-};
-
-class LeNode : public ASTNode {
-public:
-    LeNode(std::string t) : ASTNode(t) {}
-};
-
-class ConsNode : public ASTNode {
-public:
-    ConsNode(std::string t) : ASTNode(t) {}
-};
-
-class EqualNode : public ASTNode {
-public:
-    EqualNode(std::string t) : ASTNode(t) {}
-};
-
-
-// ternary
-class CondNode : public ASTNode {
-public:
-    CondNode(std::string t) : ASTNode(t) {}
-};
-
-
-// other Nodes
-class FCallNode : public ASTNode {
-public:
-    FCallNode(std::string t) : ASTNode(t) {}
-}; // maybe needs to fix AST creation in parser
-
-class LambdaNode : public ASTNode {
-public:
-    LambdaNode(std::string t) : ASTNode(t) {}
-};
-
-class FuncClosureNode : public ASTNode {
-public:
-    FuncClosureNode(std::string t) : ASTNode(t) {}
-};
-
-class LetNode : public ASTNode {
-public:
-    LetNode(std::string t) : ASTNode(t) {}
-};
-
-class LetrecNode : public ASTNode {
-public:
-    LetrecNode(std::string t) : ASTNode(t) {}
-};
-
-
-class LiteralInt : public ASTNode {
-    int value;
-
-public:
-    void printValue(bool flat = false) const override {
-        std::cout << value;
-    }
-
-    int getValue() {
-        return value;
-    }
-
-    LiteralInt(std::string t, int v) : ASTNode(t), value(v) {}
-};
-
-class LiteralBool : public ASTNode {
-    bool value;
-
-public:
-    void printValue(bool flat = false) const override {
-        if (value) {
-            std::cout << "TRUE";
-        }
-        else {
-            std::cout << "FALSE";
-        }
-    }
-
-    bool getValue() {
-        return value;
-    }
-
-    LiteralBool(std::string t, bool v) : ASTNode(t), value(v) {}
-};
-
-class ListNode : public ASTNode {
-public:
-    ListNode(std::string t) : ASTNode(t) {}
-
-    void printFlat() const override {
-        this->printValue(true);
-        std::cout << "(";
+        printValue();
         
-        for (const auto& stmt : getStatements()) {
-            stmt->printFlat();
-            std::cout << " ";
+        if (!statements.empty()) {
+            for (const auto& stmt : statements) {
+                std::cout << " ";
+                stmt->printFlat(depth + 1);
+            }
+            std::cout << ")";
         }
-
-        std::cout << ")";
     }
 };
 
-class LiteralNil : public ASTNode {
-public:
-    LiteralNil(std::string t) : ASTNode(t) {}
-};
-
-class Identifier : public ASTNode {
-    std::string value;
-
-public:
-    void printValue(bool flat = false) const override {
-        std::cout << value;
-    }
-
-    std::string getValue() {
-        return value;
-    }
-
-    Identifier(std::string t, std::string v) : ASTNode(t), value(v) {}
-};
 
 class AST {
 private:
@@ -259,22 +74,90 @@ public:
     AST() {}
     AST(std::shared_ptr<ASTNode> rootNode) : root(rootNode) {}
 
-    std::shared_ptr<ASTNode> getRoot() {
-        return root;
-    }
-
-    bool isEmpty() const {
-        return root == nullptr;
-    }
+    std::shared_ptr<ASTNode> getRoot() { return root; }
+    bool isEmpty() const { return root == nullptr; }
 
     void print(bool flat = false) const {
         if (root) {
             if (!flat) root->print(0);
             else root->printFlat();
+            std::cout << "\n";
         } else {
-            std::cout << "AST is empty.\n";
+            std::cout << "AST is empty.\n\n";
         }
     }
 };
+
+
+// unary
+class QuoteNode : public ASTNode { public: QuoteNode(std::string t) : ASTNode(t) {} };
+class CarNode : public ASTNode { public: CarNode(std::string t) : ASTNode(t) {} };
+class CdrNode : public ASTNode { public: CdrNode(std::string t) : ASTNode(t) {} };
+class AtomNode : public ASTNode { public: AtomNode(std::string t) : ASTNode(t) {} };
+
+// binary
+class AddNode : public ASTNode { public: AddNode(std::string t) : ASTNode(t) {} };
+class SubNode : public ASTNode { public: SubNode(std::string t) : ASTNode(t) {} };
+class MulNode : public ASTNode { public: MulNode(std::string t) : ASTNode(t) {} };
+class DiveNode : public ASTNode { public: DiveNode(std::string t) : ASTNode(t) {} };
+class RemNode : public ASTNode { public: RemNode(std::string t) : ASTNode(t) {} };
+class LeNode : public ASTNode { public: LeNode(std::string t) : ASTNode(t) {} };
+class ConsNode : public ASTNode { public: ConsNode(std::string t) : ASTNode(t) {} };
+class EqualNode : public ASTNode { public: EqualNode(std::string t) : ASTNode(t) {} };
+
+// ternary
+class CondNode : public ASTNode { public: CondNode(std::string t) : ASTNode(t) {} };
+
+// other Nodes
+class LambdaNode : public ASTNode { public: LambdaNode(std::string t) : ASTNode(t) {} };
+class FuncClosureNode : public ASTNode { public: FuncClosureNode(std::string t) : ASTNode(t) {} };
+class LetNode : public ASTNode { public: LetNode(std::string t) : ASTNode(t) {} };
+class LetrecNode : public ASTNode { public: LetrecNode(std::string t) : ASTNode(t) {} };
+
+
+class LiteralInt : public ASTNode {
+    int value;
+public:
+    void printValue() const override { std::cout << value; }
+    int getValue() { return value; }
+    LiteralInt(std::string t, int v) : ASTNode(t), value(v) {}
+};
+
+class LiteralBool : public ASTNode {
+    bool value;
+public:
+    void printValue() const override {
+        if (value) { std::cout << "TRUE"; }
+        else { std::cout << "FALSE"; }
+    }
+    bool getValue() { return value; }
+    LiteralBool(std::string t, bool v) : ASTNode(t), value(v) {}
+};
+
+class ListNode : public ASTNode { 
+public: 
+    ListNode(std::string t) : ASTNode(t) {}
+    void printFlat(int depth = 0) const override {
+        std::cout << "(";
+        if (!getStatements().empty()) {
+            for (const auto& stmt : getStatements()) {
+                std::cout << " ";
+                stmt->printFlat(depth);
+            }
+        }
+        std::cout << ")";
+    }
+};
+
+class LiteralNil : public ASTNode { public: LiteralNil(std::string t) : ASTNode(t) {} };
+
+class Identifier : public ASTNode {
+    std::string value;
+public:
+    void printValue() const override { std::cout << value; }
+    std::string getValue() { return value; }
+    Identifier(std::string t, std::string v) : ASTNode(t), value(v) {}
+};
+
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,28 @@ extern syntax_tree::AST analize(int argc, char* argv[]);
 int main(int argc, char* argv[])
 {   
     syntax_tree::AST ast = analize(argc, argv);
+    std::cout << "-----------------------------\n";
+    std::cout << "----Abstract syntax tree:----\n";
+    std::cout << "-----------------------------\n";
     ast.print();
 
     Emulator* e = new Emulator();
-    std::cout << "\nResult tree:\n";
-    syntax_tree::AST result = e->eval(std::move(ast));
+    syntax_tree::AST result;
+    try {
+        result = e->eval(ast);
+    }
+    catch(const std::exception& e) {
+        std::cerr << "Evaluation error: `" << e.what() << "`\n\n";
+    }
+    std::cout << "-----------------------------\n";
+    std::cout << "--------Evaluated AST:-------\n";
+    std::cout << "-----------------------------\n";
     result.print(false);
+    std::cout << "-----------------------------\n";
+    std::cout << "------Evaluated flat AST:----\n";
+    std::cout << "-----------------------------\n";
+    result.print(true);
+    
     std::cout << "\n";
 
     return 0;
